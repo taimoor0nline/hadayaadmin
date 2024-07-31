6462f24aaf03095a5d22f321b7aa56deee9a1e76
tim <taimoor.iqbal@codeninjaconsulting.com>
2024-07-31 22:59:37 +0500

    shopify fix

--- src/services/shopifyService.ts
+++ src/services/shopifyService.ts
@@ -1,27 +1,32 @@
-// src/services/shopifyService.ts
-
 import axios from 'axios';
 import { Order } from '../interfaces/Interfaces';
-
-const SHOPIFY_API_URL = 'https://testhadaya.myshopify.com/admin/api/2024-07';
-const SHOPIFY_ACCESS_TOKEN = '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';
+import API_BASE_URL from '../config/apiConfig';
 
 const shopifyService = axios.create({
-  baseURL: SHOPIFY_API_URL,
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    'X-Shopify-Access-Token': SHOPIFY_ACCESS_TOKEN,
   },
 });
 
 export const getOrders = async (): Promise<Order[]> => {
-  const response = await shopifyService.get('/orders.json?status=any');
-  return response.data.orders;
+  try {
+    const response = await shopifyService.get('/shopify/orders');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching orders:', error);
+    throw error;
+  }
 };
 
 export const getOrderById = async (orderId: string): Promise<Order> => {
-  const response = await shopifyService.get(`/orders/${orderId}.json`);
-  return response.data.order;
+  try {
+    const response = await shopifyService.get(`/shopify/orders/${orderId}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching order by ID:', error);
+    throw error;
+  }
 };
 
 export default shopifyService;
